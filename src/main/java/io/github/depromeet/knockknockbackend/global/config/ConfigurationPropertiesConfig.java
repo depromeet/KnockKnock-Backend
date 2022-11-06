961cb8ae6d73424c9cfb20fb6a0aa4321f986bf7
이찬진 <water0641@naver.com>
2022-11-06 15:11:15 +0900

    ⚙ :: configuration 프로세서 종속성 추가

--- src/main/java/io/github/depromeet/knockknockbackend/global/config/ConfigurationPropertiesConfig.java
+++ src/main/java/io/github/depromeet/knockknockbackend/global/config/ConfigurationPropertiesConfig.java
@@ -1,9 +1,10 @@
 package io.github.depromeet.knockknockbackend.global.config;
 
+import io.github.depromeet.knockknockbackend.global.property.JwtProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties
+@EnableConfigurationProperties(JwtProperties.class)
 @Configuration
 public class ConfigurationPropertiesConfig {
 }
