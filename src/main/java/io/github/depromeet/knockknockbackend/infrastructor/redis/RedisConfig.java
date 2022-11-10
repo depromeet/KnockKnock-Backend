4142246dd47721586b3286a51418d0ed0fb7da43
leeseojune53 <sung07288346@gmail.com>
2022-11-11 00:06:57 +0900

    ⚡️ :: Redis Config

--- src/main/java/io/github/depromeet/knockknockbackend/infrastructor/redis/RedisConfig.java
+++ src/main/java/io/github/depromeet/knockknockbackend/infrastructor/redis/RedisConfig.java
@@ -4,35 +4,40 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+import java.time.Duration;
+
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @Configuration
 public class RedisConfig {
-    @Value("${spring.datasource.redis.host}")
-    private String host;
 
-    @Value("${spring.datasource.redis.port}")
-    private int port;
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisConfig =
+                new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        if(redisPassword != null && !redisPassword.isBlank())
+            redisConfig.setPassword(redisPassword);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(1))
+                .shutdownTimeout(Duration.ZERO)
+                .build();
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        // redisTemplate를 받아와서 set, get, delete를 사용
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        /**
-         * setKeySerializer, setValueSerializer 설정
-         * redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
-         */
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-
-        return redisTemplate;
-    }
 }
