6945c11f5369e3015a747fb3ee4321ebd8dd97ec
이찬진 <water0641@naver.com>
2022-11-11 23:52:04 +0900

    :zap: :: 스웨거 관련  설정

---
+++ src/main/java/io/github/depromeet/knockknockbackend/global/config/SwaggerConfig.java
@@ -0,0 +1,77 @@
+package io.github.depromeet.knockknockbackend.global.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.*;
+
+/**
+ * Swagger 사용 환경을 위한 설정 파일
+ * */
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder()
+            .title("\"낙낙 서버 API문서\"")
+            .license("디프만 7팀 낙낙")
+            .licenseUrl("https://github.com/depromeet/KnockKnock-Backend")
+            .version("0.0.1")
+            .description("낙낙 서버의 API 문서 입니다.")
+            .build();
+    }
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(swaggerInfo())
+            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()))
+            .ignoredParameterTypes(AuthenticationPrincipal.class)
+            .consumes(getConsumeContentTypes())
+            .produces(getProduceContentTypes())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("io.github.knockknockbackend"))
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .build();
+    }
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+            = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+}
