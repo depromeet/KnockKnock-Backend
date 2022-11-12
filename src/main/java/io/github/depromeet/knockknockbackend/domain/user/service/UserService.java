4ce8034c14c0a0e20e7644dec710af6c8897990e
leeseojune53 <sung07288346@gmail.com>
2022-11-12 19:19:21 +0900

    ⚡️ :: changeNickname

--- src/main/java/io/github/depromeet/knockknockbackend/domain/user/service/UserService.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package io.github.depromeet.knockknockbackend.domain.user.service;
 
+import io.github.depromeet.knockknockbackend.domain.user.domain.User;
 import io.github.depromeet.knockknockbackend.domain.user.domain.repository.UserRepository;
 import io.github.depromeet.knockknockbackend.domain.user.presentation.dto.response.QueryUserByNicknameResponse;
 import io.github.depromeet.knockknockbackend.domain.user.presentation.dto.response.QueryUserByNicknameResponseElement;
+import io.github.depromeet.knockknockbackend.global.exception.UserNotFoundException;
+import io.github.depromeet.knockknockbackend.global.utils.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +29,15 @@ public class UserService {
                 .collect(Collectors.toList());
 
         return new QueryUserByNicknameResponse(result);
+    }
+
+    public void changeNickname(String nickname) {
+        User user = userRepository.findById(SecurityUtils.getCurrentUserId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        user.changeNickname(nickname);
+
+        userRepository.save(user);
     }
 
 }
