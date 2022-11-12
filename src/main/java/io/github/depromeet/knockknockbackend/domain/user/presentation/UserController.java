c432298518c900aafc9ed7547cdfad51a147c243
leeseojune53 <sung07288346@gmail.com>
2022-11-12 19:19:31 +0900

    ⚡️ :: PUT /users/nickname

--- src/main/java/io/github/depromeet/knockknockbackend/domain/user/presentation/UserController.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/user/presentation/UserController.java
@@ -1,12 +1,19 @@
 package io.github.depromeet.knockknockbackend.domain.user.presentation;
 
+import io.github.depromeet.knockknockbackend.domain.user.presentation.dto.request.ChangeNicknameRequest;
 import io.github.depromeet.knockknockbackend.domain.user.presentation.dto.response.QueryUserByNicknameResponse;
 import io.github.depromeet.knockknockbackend.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -15,9 +22,15 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/{nickname}")
+    @GetMapping("/nickname/{nickname}")
     public QueryUserByNicknameResponse queryUserByNickname(@PathVariable("nickname") String nickname) {
         return userService.queryUserByNicknameResponse(nickname);
+    }
+
+    @PutMapping("/nickname")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changeNickname(@RequestBody @Valid ChangeNicknameRequest request) {
+        userService.changeNickname(request.getNickname());
     }
 
 }
