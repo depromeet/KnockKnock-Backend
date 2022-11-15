ac79f154783db394470f3a9589e1242ef7346f34
leeseojune53 <sung07288346@gmail.com>
2022-11-16 00:55:03 +0900

    📑 :: UserUtils

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/user/UserUtils.java
@@ -0,0 +1,7 @@
+package io.github.depromeet.knockknockbackend.domain.user;
+
+import io.github.depromeet.knockknockbackend.domain.user.domain.User;
+
+public interface UserUtils {
+    User getUserById(Long id);
+}
