627b3e46c443d0d1cbf1ca6697283e1067045343
이찬진 <water0641@naver.com>
2022-11-16 14:09:14 +0900

    :zap: :: [group] 친구 그룹생성   요청 dto

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/dto/request/CreateFriendGroupRequest.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/dto/request/CreateFriendGroupRequest.java
@@ -1,5 +1,11 @@
 package io.github.depromeet.knockknockbackend.domain.group.presentation.dto.request;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateFriendGroupRequest {
+
+    // 친구목록만 받아옴 빈리스트도 가능
+    private List<Long> memberIds = new ArrayList<>();
 
 }
