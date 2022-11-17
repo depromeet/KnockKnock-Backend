12229ac9c357d7b414a7f6a98ab73dd16487152e
mybloom <baejungeun11@gmail.com>
2022-11-17 22:58:41 +0900

    📑 :: 스웨거를 이용한 API 문서 작성
    - @Tag, @SecurityRequirement 추가
    - @SecurityRequirement : access-token이 필요한 api임을 명시

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/NotificationController.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/NotificationController.java
@@ -3,6 +3,8 @@ package io.github.depromeet.knockknockbackend.domain.notification.presentation;
 import io.github.depromeet.knockknockbackend.domain.notification.domain.Notification;
 import io.github.depromeet.knockknockbackend.domain.notification.presentation.dto.response.QueryAlarmHistoryResponse;
 import io.github.depromeet.knockknockbackend.domain.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@SecurityRequirement(name = "access-token")
+@Tag(name = "알람 관련 컨트롤러", description = "")
 @RequiredArgsConstructor
 @RequestMapping("/notifications")
 @RestController
