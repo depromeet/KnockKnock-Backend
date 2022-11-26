64683b98005cfd4bde76f75ab808ef7eea133017
이찬진 <water0641@naver.com>
2022-11-26 15:11:14 +0900

    :recycle: :: [group] 업데이트 요청 dto 예시 요청값 수정

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/dto/request/UpdateGroupRequest.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/dto/request/UpdateGroupRequest.java
@@ -1,11 +1,14 @@
 package io.github.depromeet.knockknockbackend.domain.group.presentation.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateGroupRequest {
     @NotNull
     private String title;
@@ -20,5 +23,6 @@ public class UpdateGroupRequest {
 
     private String backgroundImagePath;
 
+    @Schema(defaultValue = "1")
     private Long categoryId;
 }
