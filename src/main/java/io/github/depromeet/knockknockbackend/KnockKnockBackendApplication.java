26524607b230940060552b11c6b1f79ff82e42b1
이찬진 <water0641@naver.com>
2022-11-27 22:07:52 +0900

    :zap: :: baseTimeEntity 추가

--- src/main/java/io/github/depromeet/knockknockbackend/KnockKnockBackendApplication.java
+++ src/main/java/io/github/depromeet/knockknockbackend/KnockKnockBackendApplication.java
@@ -2,7 +2,10 @@ package io.github.depromeet.knockknockbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class KnockKnockBackendApplication {
 
