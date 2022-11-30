c69ba3fab2e201311f8a9f64e28e9fd2bce4d838
이찬진 <water0641@naver.com>
2022-11-30 12:11:10 +0900

    :zap: :: admissionFacade 추가

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/facade/AdmissionFacade.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/facade/AdmissionFacade.java
@@ -1,8 +1,10 @@
 package io.github.depromeet.knockknockbackend.domain.group.facade;
 
 
+import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
 import io.github.depromeet.knockknockbackend.domain.group.presentation.dto.response.AdmissionInfoDto;
 import io.github.depromeet.knockknockbackend.domain.group.presentation.dto.response.AdmissionInfoListResponse;
+import io.github.depromeet.knockknockbackend.domain.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,15 +14,27 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class AdmissionFacade {
 
+    private final AdmissionService admissionService;
+    private final GroupService groupService;
+
+
     public AdmissionInfoDto requestAdmission(Long groupId) {
+        Group group = groupService.queryGroup(groupId);
+        return admissionService.requestAdmission(group);
     }
 
     public AdmissionInfoListResponse getAdmissions(Long groupId) {
+        Group group = groupService.queryGroup(groupId);
+        return admissionService.getAdmissions(group);
     }
 
     public AdmissionInfoDto acceptAdmission(Long groupId, Long admissionId) {
+        Group group = groupService.queryGroup(groupId);
+        return admissionService.acceptAdmission(group,admissionId);
     }
 
     public AdmissionInfoDto refuseAdmission(Long groupId, Long admissionId) {
+        Group group = groupService.queryGroup(groupId);
+        return admissionService.refuseAdmission(group,admissionId);
     }
 }
