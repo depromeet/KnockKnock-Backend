df6a7020dc8db77ecdc6b83f2bc88bc41ffef441
mybloom <baejungeun11@gmail.com>
2022-12-05 19:55:47 +0900

    ♻ :: DeviceToken 엔티티에 User 연관관계 설정
    - userId 에서 User로 연관관계 설정

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/DeviceToken.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/DeviceToken.java
@@ -1,11 +1,15 @@
 package io.github.depromeet.knockknockbackend.domain.notification.domain;
 
+import io.github.depromeet.knockknockbackend.domain.user.domain.User;
 import io.github.depromeet.knockknockbackend.global.database.BaseTimeEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,7 +29,9 @@ public class DeviceToken extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long userId;
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 
     private String deviceId;
 
@@ -35,7 +41,7 @@ public class DeviceToken extends BaseTimeEntity {
 
     public static DeviceToken of(Long userId, String deviceId, String deviceToken) {
         return DeviceToken.builder()
-            .userId(userId)
+            .user(User.of(userId))
             .deviceId(deviceId)
             .token(deviceToken)
             .build();
