4e00620d3a56d5e543f3bc8e6755f2a4bde333c2
이찬진 <water0641@naver.com>
2022-12-05 20:28:37 +0900

    :recycle: :: [groupRepository] 최근 알림 기준 정렬 group slice native query

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/GroupRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/GroupRepository.java
@@ -4,18 +4,25 @@ import io.github.depromeet.knockknockbackend.domain.group.domain.Category;
 import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
 import io.github.depromeet.knockknockbackend.domain.group.domain.GroupType;
 import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
-    List<Group> findAllByGroupType(GroupType groupType);
 
-    List<Group> findAllByCategory(Category category);
+    @Query(value = "select G.* , N.send_at from tbl_group as G left outer join tbl_notification as N on "
+        + "N.id = (select Top 1 id from tbl_notification where group_id = G.id order by id DESC limit 1)"
+        + "where G.group_type = :#{#groupType.getType()} order by coalesce(N.send_at , G.created_date) DESC" , nativeQuery = true)
+    Slice<Group> findSliceByGroupType(@Param("groupType") GroupType groupType, PageRequest pageRequest);
 
-
-
-    List<Group> findAllByGroupTypeAndCategory(GroupType groupType,Category category);
+    @Query(value = "select G.* , N.send_at from tbl_group as G left outer join tbl_notification as N on "
+        + "N.id = (select id from tbl_notification where group_id = G.id order by id DESC limit 1)"
+        + "where G.group_type = :#{#groupType.getType()} and G.category_id = :category order by coalesce(N.send_at , G.created_date) DESC" , nativeQuery = true)
+    Slice<Group> findSliceByGroupTypeAndCategory(@Param("groupType") GroupType groupType ,@Param("category") Category category , Pageable pageable);
 
     List<Group> findByGroupTypeAndTitleContaining(GroupType groupType,String searchString);
 }
