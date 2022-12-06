7031ddea0ae8b89a13a33f6af00a41fe2573c43b
mybloom <baejungeun11@gmail.com>
2022-12-06 13:49:06 +0900

    📑 :: 인스턴스 알림을 위한 requestDTO생성

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/dto/request/SendInstanceRequest.java
@@ -0,0 +1,12 @@
+package io.github.depromeet.knockknockbackend.domain.notification.presentation.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class SendInstanceRequest {
+
+    private Long groupId;
+    private String content;
+    private String imageUrl;
+
+}
