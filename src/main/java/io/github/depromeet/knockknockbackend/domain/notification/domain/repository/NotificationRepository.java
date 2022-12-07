4aa98e427d88e1f5709b300e24629ea1e4ee6af2
mybloom <baejungeun11@gmail.com>
2022-12-07 13:41:35 +0900

    📑 :: Repository 에 알림방 내 알림 리스트 조회 메서드 추가
    - findAllByGroupId()

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/NotificationRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/NotificationRepository.java
@@ -3,10 +3,14 @@ package io.github.depromeet.knockknockbackend.domain.notification.domain.reposit
 import io.github.depromeet.knockknockbackend.domain.notification.domain.Notification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
 
     Page<Notification> findAllByReceiveUserId(Long receiveUserId, Pageable pageable);
+
+    Slice<Notification> findAllByGroupId(Long groupId, Pageable pageable);
+
 
 }
