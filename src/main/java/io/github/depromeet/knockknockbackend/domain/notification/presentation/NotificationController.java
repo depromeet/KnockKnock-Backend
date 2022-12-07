568dd04c7d11283d5bdb928c04a75a586226abbb
mybloom <baejungeun11@gmail.com>
2022-12-07 14:49:01 +0900

    📑 :: 스웨거를 이용해 API 명세 작성

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/NotificationController.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/NotificationController.java
@@ -4,6 +4,7 @@ import io.github.depromeet.knockknockbackend.domain.notification.presentation.dt
 import io.github.depromeet.knockknockbackend.domain.notification.presentation.dto.request.SendInstanceRequest;
 import io.github.depromeet.knockknockbackend.domain.notification.presentation.dto.response.QueryAlarmHistoryResponse;
 import io.github.depromeet.knockknockbackend.domain.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -28,24 +29,28 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
+    @Operation(summary = "최신 푸쉬알림 리스트")
     @GetMapping("/history")
     public QueryAlarmHistoryResponse queryAlarmHistoryByUserId(
         @PageableDefault(size = 5, sort = "sendAt", direction = Direction.DESC) Pageable pageable) {
         return notificationService.queryAlarmHistoryByUserId(pageable);
     }
 
+    @Operation(summary = "FCM 토큰 등록")
     @PostMapping("/token")
     public ResponseEntity<Void> registerFcmToken(@RequestBody RegisterFcmTokenRequest request) {
         notificationService.registerFcmToken(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "즉시 알림 발송")
     @PostMapping("/instance")
     public ResponseEntity<Void> sendInstance(@RequestBody SendInstanceRequest request) {
         notificationService.sendInstance(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "알림방 푸쉬알림 리스트")
     @GetMapping("/history/{group_id}")
     public QueryAlarmHistoryResponse queryHistoryByGroupId(
         @PageableDefault(size = 20, sort = "sendAt", direction = Direction.DESC) Pageable pageable,
