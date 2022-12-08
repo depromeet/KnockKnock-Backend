d769a629e57d4b8e5b7ca51875ad02ae1ee598c8
mybloom <baejungeun11@gmail.com>
2022-12-08 13:44:10 +0900

    📑 :: 리액션 등록/수정 API작성
    - 컨트롤러, 서비스, ReqestDTO

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/reaction/service/ReactionService.java
@@ -0,0 +1,46 @@
+package io.github.depromeet.knockknockbackend.domain.reaction.service;
+
+import io.github.depromeet.knockknockbackend.domain.notification.domain.Notification;
+import io.github.depromeet.knockknockbackend.domain.reaction.domain.NotificationReaction;
+import io.github.depromeet.knockknockbackend.domain.reaction.domain.Reaction;
+import io.github.depromeet.knockknockbackend.domain.reaction.exception.ReactionForbiddenException;
+import io.github.depromeet.knockknockbackend.domain.reaction.presentation.dto.request.RegisterReactionRequest;
+import io.github.depromeet.knockknockbackend.domain.reaction.domain.repository.NotificationReactionRepository;
+import io.github.depromeet.knockknockbackend.global.utils.security.SecurityUtils;
+import io.github.depromeet.knockknockbackend.global.utils.user.UserUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ReactionService {
+
+    private final NotificationReactionRepository notificationReactionRepository;
+    private final UserUtils userUtils;
+
+    @Transactional
+    public void registerReaction(RegisterReactionRequest request) {
+        if (request.getNotificationReactionId() != null) {
+            isMyReactionTheNotification(request.getNotificationReactionId());
+        }
+
+        notificationReactionRepository.save(
+            NotificationReaction.of(
+                request.getNotificationReactionId(),
+                Notification.of(request.getNotificationId()),
+                Reaction.of(request.getReactionId()),
+                userUtils.getUserFromSecurityContext()
+            ));
+    }
+
+    private void isMyReactionTheNotification(Long notificationReactionId) {
+        notificationReactionRepository.findById(notificationReactionId)
+            .ifPresent(notificationReaction -> {
+                if (!notificationReaction.getUser().getId().equals(SecurityUtils.getCurrentUserId()))
+                    throw ReactionForbiddenException.EXCEPTION;
+            });
+    }
+
+
+}
