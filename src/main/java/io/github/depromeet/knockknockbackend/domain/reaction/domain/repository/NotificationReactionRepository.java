49adcf4c03f28be12a2481530ea5aaed89c859a0
mybloom <baejungeun11@gmail.com>
2022-12-08 13:40:35 +0900

    📑 :: NotificationReactionRepository 생성

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/reaction/domain/repository/NotificationReactionRepository.java
@@ -0,0 +1,9 @@
+package io.github.depromeet.knockknockbackend.domain.reaction.domain.repository;
+
+import io.github.depromeet.knockknockbackend.domain.reaction.domain.NotificationReaction;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NotificationReactionRepository extends CrudRepository<NotificationReaction, Long> {
+
+
+}
