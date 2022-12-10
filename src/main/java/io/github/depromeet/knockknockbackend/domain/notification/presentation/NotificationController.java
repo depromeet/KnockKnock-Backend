98e8298d5a253d81b2bc6d7a80ee89b43eb9aa51
mybloom <baejungeun11@gmail.com>
2022-12-10 16:30:52 +0900

    ♻ :: 반환값을 ResponseEntity<Void> 를 void로 변경, @ResponseStatus 추가
    - @ResponseStatus(HttpStatus.CREATED)

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/NotificationController.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/NotificationController.java
@@ -11,11 +11,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @SecurityRequirement(name = "access-token")
@@ -33,16 +33,16 @@ public class NotificationController {
         return notificationService.queryAlarmHistoryByUserId(pageable);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/token")
-    public ResponseEntity<Void> registerFcmToken(@RequestBody RegisterFcmTokenRequest request) {
+    public void registerFcmToken(@RequestBody RegisterFcmTokenRequest request) {
         notificationService.registerFcmToken(request);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/instance")
-    public ResponseEntity<Void> sendInstance(@RequestBody SendInstanceRequest request) {
+    public void sendInstance(@RequestBody SendInstanceRequest request) {
         notificationService.sendInstance(request);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 
