58c64129ae4b9f2949819327cdaeeb2d83a64e00
이찬진 <water0641@naver.com>
2022-12-12 21:57:34 +0900

    :recycle: :: findSliceByGroupTypeAndCategory querydsl 로 변경, 조인 on 조건 최적화

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/CustomGroupRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/CustomGroupRepository.java
@@ -1,14 +1,19 @@
 package io.github.depromeet.knockknockbackend.domain.group.domain.repository;
 
+import io.github.depromeet.knockknockbackend.domain.group.domain.Category;
 import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
 import io.github.depromeet.knockknockbackend.domain.group.domain.GroupType;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.query.Param;
 
 public interface CustomGroupRepository {
 
 
-    Slice<Group> findSliceByGroupType(@Param("groupType") GroupType groupType, PageRequest pageRequest);
+    Slice<Group> findSliceByGroupType(GroupType groupType, Pageable pageable);
+
+    Slice<Group> findSliceByGroupTypeAndCategory(GroupType groupType, Category category, Pageable pageable);
+
 
 }
--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/CustomGroupRepositoryImpl.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/CustomGroupRepositoryImpl.java
@@ -1,16 +1,16 @@
 package io.github.depromeet.knockknockbackend.domain.group.domain.repository;
 
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.github.depromeet.knockknockbackend.domain.group.domain.Category;
 import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
 import io.github.depromeet.knockknockbackend.domain.group.domain.GroupType;
-import io.github.depromeet.knockknockbackend.domain.notification.domain.QNotification;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import static io.github.depromeet.knockknockbackend.domain.group.domain.QGroup.group;
@@ -22,30 +22,53 @@ public class CustomGroupRepositoryImpl implements CustomGroupRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    @Override
-    public Slice<Group> findSliceByGroupType(@Param("groupType") GroupType groupType, PageRequest pageRequest) {
-        List<Group> groups = queryFactory.selectFrom(group)
-            .leftJoin(group.notifications, notification)
+    private boolean hasNext(List<Group> groups, Pageable pageable) {
+        boolean hasNext = false;
+        if (groups.size() > pageable.getPageSize()) {
+            groups.remove(pageable.getPageSize());
+            hasNext = true;
+        }
+        return hasNext;
+    }
+
+    private JPAQuery<Group> getGroupWithRecentNotification() {
+        return queryFactory.selectFrom(group)
+            .leftJoin(notification)
             .on(notification.id.eq(
                 JPAExpressions.select(notification.id.max())
                     .from(notification)
                     .where(notification.group.id.eq(group.id))
                     .orderBy(notification.id.desc())
-            ))
+            ));
+    }
+    @Override
+    public Slice<Group> findSliceByGroupType(GroupType groupType, Pageable pageable) {
+        List<Group> groups = getGroupWithRecentNotification()
             .where(group.groupType.eq(groupType))
             .orderBy(notification.sendAt.coalesce(group.createdDate).desc())
-            .offset(pageRequest.getOffset())
-            .limit(pageRequest.getPageSize() + 1)
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize() + 1)
+            .fetch();
+        
+        return new SliceImpl<>(groups, pageable, hasNext(groups, pageable));
+    }
+
+    @Override
+    public Slice<Group> findSliceByGroupTypeAndCategory(GroupType groupType, Category category, Pageable pageable) {
+        List<Group> groups = getGroupWithRecentNotification()
+            .where(group.groupType.eq(groupType)
+                .and(group.category.eq(category)))
+            .orderBy(notification.sendAt.coalesce(group.createdDate).desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize() + 1)
             .fetch();
 
-        boolean hasNext = false;
-        if (groups.size() > pageRequest.getPageSize()) {
-            groups.remove(pageRequest.getPageSize());
-            hasNext = true;
-        }
+        boolean hasNext = hasNext(groups, pageable);
 
-        return new SliceImpl<>(groups, pageRequest, hasNext);
-    };
+        return new SliceImpl<>(groups, pageable, hasNext);
+    }
+
+
 
 
 }
