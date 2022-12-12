68ac6adf10328e2fced3a24b018f7416e8a46229
이찬진 <water0641@naver.com>
2022-12-13 00:09:47 +0900

    📑 :: groupUtils 추가

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/service/GroupUtils.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/service/GroupUtils.java
@@ -4,4 +4,8 @@ import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
 
 public interface GroupUtils {
     Group queryGroup(Long groupId);
+
+    void validReqUserIsGroupHost(Group group, Long userId);
+
+    void validUserIsAlreadyEnterGroup(Group group, Long userId);
 }
