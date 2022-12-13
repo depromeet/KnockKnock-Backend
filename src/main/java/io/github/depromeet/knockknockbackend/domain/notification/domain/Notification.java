71ae28c5a3b37ca7d1d5b8e79b8a7e6acf6dbaf1
mybloom <baejungeun11@gmail.com>
2022-12-13 19:19:51 +0900

    📑 :: Notification 에서 reaction 정보추출 메서드 추가

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/Notification.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/Notification.java
@@ -1,9 +1,21 @@
 package io.github.depromeet.knockknockbackend.domain.notification.domain;
 
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
+import io.github.depromeet.knockknockbackend.domain.notification.domain.vo.NotificationBaseInfoVo;
+import io.github.depromeet.knockknockbackend.domain.notification.domain.vo.NotificationReactionBaseInfoVo;
+import io.github.depromeet.knockknockbackend.domain.notification.domain.vo.NotificationReactionInfoVo;
+import io.github.depromeet.knockknockbackend.domain.reaction.domain.NotificationReaction;
 import io.github.depromeet.knockknockbackend.domain.user.domain.User;
 import io.github.depromeet.knockknockbackend.global.database.BaseTimeEntity;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -11,6 +23,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
@@ -73,4 +86,51 @@ public class Notification extends BaseTimeEntity {
             .sendAt(sendAt)
             .build();
     }
+
+    public NotificationBaseInfoVo getNotificationBaseInfoVo(Long userId) {
+        return NotificationBaseInfoVo.builder()
+            .notificationId(id)
+            .title(title)
+            .content(content)
+            .imageUrl(imageUrl)
+            .sendAt(sendAt)
+            .sendUserId(sendUser.getId())
+            .notificationReactionBaseInfoVo(getNotificationReactionBaseInfoVo(userId))
+            .build();
+    }
+
+    private NotificationReactionBaseInfoVo getNotificationReactionBaseInfoVo(Long userId) {
+        return NotificationReactionBaseInfoVo.builder()
+            .myReactionId(getMyReactionId(userId))
+            .reactionInfo(getNotificationReactionInfoVo())
+            .build();
+    }
+
+    private List<NotificationReactionInfoVo> getNotificationReactionInfoVo() {
+        Map<Long, Long> countPerReactions = notificationReactions.stream()
+            .collect(
+                groupingBy(notificationReaction ->
+                    notificationReaction.getReaction().getId(), counting())
+            );
+
+        return countPerReactions.entrySet().stream()
+            .map(countOfReactionId ->
+                NotificationReactionInfoVo.builder()
+                    .reactionId(countOfReactionId.getKey())
+                    .reactionCount(countOfReactionId.getValue().intValue())
+                    .build()
+            ).collect(Collectors.toList());
+    }
+
+    private Long getMyReactionId(Long userId) {
+        Optional<NotificationReaction> result = notificationReactions.stream()
+            .filter(notificationReaction -> userId.equals(notificationReaction.getUser().getId()))
+            .findAny();
+
+        if(result.isPresent()){
+            return result.get().getReaction().getId();
+        }
+        return null;
+    }
+
 }
