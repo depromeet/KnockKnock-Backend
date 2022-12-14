c5af2c2c21d18d7c25aff0e5c0f587c7d80fd892
mybloom <baejungeun11@gmail.com>
2022-12-14 13:15:31 +0900

    📑 :: NotificatonRepository 에 groupId에 해당하는 Notification 가져오는 findAllByGroupId() 생성

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/NotificationRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/NotificationRepository.java
@@ -3,12 +3,12 @@ package io.github.depromeet.knockknockbackend.domain.notification.domain.reposit
 import io.github.depromeet.knockknockbackend.domain.notification.domain.Notification;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
 
-    Slice<Notification> findAllByReceiveUserId(Long receiveUserId, Pageable pageable);
-
+    @EntityGraph(attributePaths = {"group"})
     Slice<Notification> findAllByGroupId(Long groupId, Pageable pageable);
 
 
