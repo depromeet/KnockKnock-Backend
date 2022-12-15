6fe114afa6f7c1e07719cf38858ae54f29ed56c4
이찬진 <water0641@naver.com>
2022-12-16 02:54:46 +0900

    :zap: :: famous category 컨트롤러 추가

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/GroupController.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/GroupController.java
@@ -122,6 +122,11 @@ public class GroupController {
         return categoryService.findAllCategory();
     }
 
+    @GetMapping("/categories/famous")
+    public CategoryListResponse getFamousCategory(){
+        return categoryService.findFamousCategory();
+    }
+
     //TODO : 관리자권한 필요
     @PostMapping("/categories")
     public CategoryDto createCategory(@RequestBody @Valid CreateCategoryRequest createCategoryRequest){
