7e8f996499b001a1242cc265021f4bbaf7c11c05
mybloom <baejungeun11@gmail.com>
2022-12-15 15:49:40 +0900

    📑 :: GroupRepository에 findByGroupIdAndUserId() 추가

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/CustomGroupUserRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/domain/repository/CustomGroupUserRepository.java
@@ -2,6 +2,7 @@ package io.github.depromeet.knockknockbackend.domain.group.domain.repository;
 
 import io.github.depromeet.knockknockbackend.domain.group.domain.GroupType;
 import io.github.depromeet.knockknockbackend.domain.group.domain.GroupUser;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
@@ -10,5 +11,7 @@ public interface CustomGroupUserRepository {
     Slice<GroupUser> findJoinedGroupUser(Long reqUserId, Pageable pageable);
 
     Slice<GroupUser> findJoinedGroupUserByGroupType(Long reqUserId, GroupType groupType , Pageable pageable);
+
+    Optional<GroupUser> findByGroupIdAndUserId(Long groupId, Long userId);
 
 }
