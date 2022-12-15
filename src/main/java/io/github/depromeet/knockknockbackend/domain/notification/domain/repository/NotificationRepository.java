ebb94660b7ec7c1d7abc5a206ca7cbb1e5d1868d
mybloom <baejungeun11@gmail.com>
2022-12-15 15:47:02 +0900

    📑 :: NotificationRepository.findById() 추가

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/NotificationRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/NotificationRepository.java
@@ -1,12 +1,17 @@
 package io.github.depromeet.knockknockbackend.domain.notification.domain.repository;
 
 import io.github.depromeet.knockknockbackend.domain.notification.domain.Notification;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
 
     Page<Notification> findAllByReceiveUserId(Long receiveUserId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"group"})
+    Optional<Notification> findById(Long notificationId);
 
 }
