dd19cddf71defdb5f88c7c45615285652d9950c2
이찬진 <water0641@naver.com>
2022-12-17 01:15:33 +0900

    :recycle: :: 역직렬화를 위한 noArg 생성자 추가

--- src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/dto/response/CategoryListResponse.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/group/presentation/dto/response/CategoryListResponse.java
@@ -3,12 +3,13 @@ package io.github.depromeet.knockknockbackend.domain.group.presentation.dto.resp
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CategoryListResponse {
-
 
     List<CategoryDto> categories;
 }
