988a1e40e11a4c02b512e8e2dfb93374bff821cc
mybloom <baejungeun11@gmail.com>
2022-12-17 22:48:14 +0900

    📑 :: 차단유저 정보 포함한 송신자의 fcm token 조회 쿼리 생성
    - queryDSL이용

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/CustomNotificationRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/CustomNotificationRepository.java
@@ -1,11 +1,15 @@
 package io.github.depromeet.knockknockbackend.domain.notification.domain.repository;
 
+import io.github.depromeet.knockknockbackend.domain.notification.domain.DeviceToken;
 import io.github.depromeet.knockknockbackend.domain.notification.domain.Notification;
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface CustomNotificationRepository {
 
     Slice<Notification> findSliceFromStorage(Long userId, Long groupId, Integer periodOfMonth, Pageable pageable);
+
+    List<DeviceToken> findTokenByGroupAndOptionAndNonBlock(Long userId, Long groupId, Boolean nightOption);
 
 }
