1036283904fed9418e9634a283d3109d8270b26d
mybloom <baejungeun11@gmail.com>
2022-12-17 13:08:35 +0900

    🐛 :: 리액션 저장시 DB unique 권한에 위배되는 부분 익셉션 발생 코드 삽입
    - save 시 notification_id, user_id 에 해당하는 데이터 있을 경우 익셉션 발생
    - ReactionAlreadyExistException 발생

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/reaction/exception/ReactionAlreadyExistException.java
@@ -0,0 +1,12 @@
+package io.github.depromeet.knockknockbackend.domain.reaction.exception;
+
+import io.github.depromeet.knockknockbackend.global.error.exception.ErrorCode;
+import io.github.depromeet.knockknockbackend.global.error.exception.KnockException;
+
+public class ReactionAlreadyExistException extends KnockException {
+    public static final KnockException EXCEPTION = new ReactionAlreadyExistException();
+
+    private ReactionAlreadyExistException() {
+        super(ErrorCode.REACTION_ALREADY_EXIST);
+    }
+}
