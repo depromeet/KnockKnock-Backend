5a352f3d900f3fd66bd9a56067f02c9bdb57116d
이찬진 <water0641@naver.com>
2022-12-17 15:47:43 +0900

    :zap: :: validation 오류 추가

--- src/main/java/io/github/depromeet/knockknockbackend/global/error/exception/ErrorCode.java
+++ src/main/java/io/github/depromeet/knockknockbackend/global/error/exception/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
 
     EXAMPLE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "EXAMPLE-404-1", "Example Not Found."),
 
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST.value(), "GLOBAL-400-1", "validation error"),
+
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED.value(), "GLOBAL-401-1", "Expired Jwt Token."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), "GLOBAL-401-1", "Invalid Jwt Token."),
 
