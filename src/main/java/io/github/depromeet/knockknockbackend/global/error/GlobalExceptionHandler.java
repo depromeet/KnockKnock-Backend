c075d2f5148710b229bf894bf286ceb84e0bb9b4
이찬진 <water0641@naver.com>
2022-12-17 16:20:12 +0900

    :recycle: :: 필드 : 에러메시지 형식 json string 리턴

--- src/main/java/io/github/depromeet/knockknockbackend/global/error/GlobalExceptionHandler.java
+++ src/main/java/io/github/depromeet/knockknockbackend/global/error/GlobalExceptionHandler.java
@@ -1,10 +1,17 @@
 package io.github.depromeet.knockknockbackend.global.error;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.depromeet.knockknockbackend.global.error.exception.ErrorCode;
 import io.github.depromeet.knockknockbackend.global.error.exception.KnockException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,15 +22,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e)
+        throws JsonProcessingException {
         ErrorCode validationError = ErrorCode.VALIDATION_ERROR;
+        List<FieldError> errors = e.getBindingResult().getFieldErrors();
+
+        Map<String, Object> fieldAndErrorMessages = errors.stream()
+            .collect(Collectors.toMap(
+                FieldError::getField, FieldError::getDefaultMessage)
+            );
+
+        String errorsToJsonString = new ObjectMapper().writeValueAsString(fieldAndErrorMessages);
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(new ErrorResponse(validationError.getStatus(),
                 validationError.getCode(),
-                e.getMessage())
+                errorsToJsonString)
             );
     }
+
     @ExceptionHandler(KnockException.class)
     public ResponseEntity<ErrorResponse> KnockExceptionHandler(KnockException e) {
         ErrorCode code = e.getErrorCode();
