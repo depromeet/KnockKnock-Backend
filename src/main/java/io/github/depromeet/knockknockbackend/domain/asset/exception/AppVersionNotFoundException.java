32a478b40bdcf10f1e2c599381938144d15f6041
이찬진 <water0641@naver.com>
2022-12-17 20:59:26 +0900

    :zap: :: [swaggerConfig] operationCustomizer disableSecurity , tag 하나만 설정가능하도록 변경

--- src/main/java/io/github/depromeet/knockknockbackend/domain/asset/exception/AppVersionNotFoundException.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/asset/exception/AppVersionNotFoundException.java
@@ -3,12 +3,12 @@ package io.github.depromeet.knockknockbackend.domain.asset.exception;
 import io.github.depromeet.knockknockbackend.global.error.exception.ErrorCode;
 import io.github.depromeet.knockknockbackend.global.error.exception.KnockException;
 
-public class BackgroundImageNotFoundException extends KnockException {
+public class AppVersionNotFoundException extends KnockException {
 
-    public static final KnockException EXCEPTION = new BackgroundImageNotFoundException();
+    public static final KnockException EXCEPTION = new AppVersionNotFoundException();
 
-    private BackgroundImageNotFoundException() {
-        super(ErrorCode.BACKGROUND_NOT_FOUND);
+    private AppVersionNotFoundException() {
+        super(ErrorCode.APP_VERSION_NOT_FOUND);
     }
 
 }
