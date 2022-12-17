9c04fa27e1bfa9a005f3373f62ee9cd106fe8d03
이찬진 <water0641@naver.com>
2022-12-17 20:55:38 +0900

    :zap: :: 앱 버전 체크 api 추가

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/asset/domain/repository/AppVersionRepository.java
@@ -0,0 +1,2 @@
+package io.github.depromeet.knockknockbackend.domain.asset.domain.repository;public interface AppVersionRepository {
+}
---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/asset/presentation/dto/response/AppVersionResponse.java
@@ -0,0 +1,2 @@
+package io.github.depromeet.knockknockbackend.domain.asset.presentation.dto.response;public class AppVersionResponse {
+}
