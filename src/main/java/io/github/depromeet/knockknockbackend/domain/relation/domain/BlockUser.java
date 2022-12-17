149dd9e3d6ac11f1017ce3efc0bd7fe669772ad0
mybloom <baejungeun11@gmail.com>
2022-12-17 22:47:04 +0900

    📑 :: BlockUser 엔티티 생성

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/relation/domain/BlockUser.java
@@ -0,0 +1,38 @@
+package io.github.depromeet.knockknockbackend.domain.relation.domain;
+
+import io.github.depromeet.knockknockbackend.domain.user.domain.User;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_block_user")
+@Entity
+public class BlockUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "user_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @JoinColumn(name = "blocked_user_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private User blockedUser;
+
+}
