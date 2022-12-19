d902818e6492cd7c5db3c80f5adb4b3d8753dd76
mybloom <baejungeun11@gmail.com>
2022-12-19 17:43:09 +0900

    📑 :: 예약 알림 엔티티 NotificationReservation 생성

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/Reservation.java
@@ -0,0 +1,37 @@
+package io.github.depromeet.knockknockbackend.domain.notification.domain;
+
+
+import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
+import io.github.depromeet.knockknockbackend.domain.user.domain.User;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_reservation")
+@Entity
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime sendAt;
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+
+    @JoinColumn(name = "group_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Group group;
+
+    @JoinColumn(name = "send_user_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private User sendUser;
+}
