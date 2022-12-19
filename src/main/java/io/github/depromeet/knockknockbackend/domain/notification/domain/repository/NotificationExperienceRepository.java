86e84994991c1010c11fe7446ca118a2e3eb5294
mybloom <baejungeun11@gmail.com>
2022-12-19 12:32:28 +0900

    📑 :: 똑똑 미리체험용 repository 생성
    - NotificationExperienceRepository

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/NotificationExperienceRepository.java
@@ -0,0 +1,8 @@
+package io.github.depromeet.knockknockbackend.domain.notification.domain.repository;
+
+
+import io.github.depromeet.knockknockbackend.domain.notification.domain.NotificationExperience;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NotificationExperienceRepository
+        extends CrudRepository<NotificationExperience, Long> {}
