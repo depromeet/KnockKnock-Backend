320c00f2856db79558c60f2ea8beee9a53e58250
이찬진 <water0641@naver.com>
2022-12-21 04:32:08 +0900

    📑 :: OIDC public key Response

---
+++ src/main/java/io/github/depromeet/knockknockbackend/global/utils/api/dto/response/OIDCPublicKeysResponse.java
@@ -0,0 +1,10 @@
+package io.github.depromeet.knockknockbackend.global.utils.api.dto.response;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor
+public class OIDCPublicKeysResponse {
+  List<OIDCPublicKeyDto> keys;
+}
