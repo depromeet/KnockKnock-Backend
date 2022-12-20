9e2b63729a33356c11a99ec1b52a624efab7c4fa
이찬진 <water0641@naver.com>
2022-12-21 05:29:50 +0900

    📑 :: OIDCDecodePayload

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/credential/service/OIDCDecodePayload.java
@@ -0,0 +1,15 @@
+package io.github.depromeet.knockknockbackend.domain.credential.service;
+
+import lombok.Getter;
+
+@Getter
+public class OIDCDecodePayload {
+    /** issuer ex https://kauth.kakao.com */
+    private String iss;
+    /** client id */
+    private String aud;
+    /** oauth provider account unique id */
+    private String sub;
+
+    private String email;
+}
