9c97d9649fd50753d1c490e84a2d1d2a0e0022ed
Leeseojune <sung07288346@gmail.com>
2022-12-22 02:00:05 +0900

    📑 :: QueryAlarmCountResponse

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/alarm/presentation/dto/response/QueryAlarmCountResponse.java
@@ -0,0 +1,12 @@
+package io.github.depromeet.knockknockbackend.domain.alarm.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class QueryAlarmCountResponse {
+
+    private final int count;
+
+}
