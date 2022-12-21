19d099ef61f2813b663138b1ef38df4e4e4c3308
이찬진 <water0641@naver.com>
2022-12-21 18:53:01 +0900

    :zap: :: 정상인 상태가 아닌 유저 토큰 발급 제한

--- src/main/java/io/github/depromeet/knockknockbackend/global/error/exception/ErrorCode.java
+++ src/main/java/io/github/depromeet/knockknockbackend/global/error/exception/ErrorCode.java
@@ -78,7 +78,9 @@ public enum ErrorCode {
     PROFILE_IMAGE_NOT_FOUND(404, "ASSET-404-1", "PROFILE Not Found"),
     BACKGROUND_NOT_FOUND(404, "ASSET-404-2", "BACKGROUND Not Found"),
     THUMBNAIL_NOT_FOUND(404, "ASSET-404-3", "THUMBNAIL Not Found"),
-    APP_VERSION_NOT_FOUND(404, "ASSET-404-4", "APP_VERSION Not Found");
+    APP_VERSION_NOT_FOUND(404, "ASSET-404-4", "APP_VERSION Not Found"),
+
+    USER_CREDENTIAL_FORBIDDEN(403,"CREDENTIAL-403-1" , "user status is not normal");
 
     private int status;
     private String code;
