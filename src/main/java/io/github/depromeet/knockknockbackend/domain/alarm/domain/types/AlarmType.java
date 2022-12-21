c2cb7b167df1a683e5b55cb8db811d3605a3e859
Leeseojune <sung07288346@gmail.com>
2022-12-22 01:59:29 +0900

    📑 :: AlarmType

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/alarm/domain/types/AlarmType.java
@@ -0,0 +1,14 @@
+package io.github.depromeet.knockknockbackend.domain.alarm.domain.types;
+
+public enum AlarmType {
+
+    FRIEND_REQUEST,
+    GROUP_INVITE,
+    GROUP_REQUEST,
+
+    REACTION,
+
+    FRIEND_ACCEPT,
+    GROUP_ACCEPT
+
+}
