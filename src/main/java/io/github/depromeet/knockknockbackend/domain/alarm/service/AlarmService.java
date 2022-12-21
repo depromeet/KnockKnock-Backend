ffd8ad2069c19470878531aaf4fb0c7ae2e8f1d6
Leeseojune <sung07288346@gmail.com>
2022-12-22 02:00:15 +0900

    📑 :: AlarmService

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/alarm/service/AlarmService.java
@@ -0,0 +1,34 @@
+package io.github.depromeet.knockknockbackend.domain.alarm.service;
+
+import io.github.depromeet.knockknockbackend.domain.alarm.domain.repository.AlarmRepository;
+import io.github.depromeet.knockknockbackend.domain.alarm.presentation.dto.response.QueryAlarmListResponse;
+import io.github.depromeet.knockknockbackend.domain.alarm.presentation.dto.response.QueryAlarmListResponseElement;
+import io.github.depromeet.knockknockbackend.global.utils.security.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class AlarmService {
+
+    private final AlarmRepository alarmRepository;
+
+    public QueryAlarmListResponse queryList() {
+        List<QueryAlarmListResponseElement> result = alarmRepository.findByReceiveUserIdAndIsActivateIsTrue(SecurityUtils.getCurrentUserId())
+                .stream().map(alarm ->
+                    new QueryAlarmListResponseElement(alarm.getSendUserId(), alarm.getSendUserProfile(), alarm.getTitle(), alarm.getContent(), alarm.getType(), alarm.getCreatedAt())
+                ).collect(Collectors.toList());
+
+        //TODO 알림 만료처리
+
+        return new QueryAlarmListResponse(result);
+    }
+
+    public int queryCount() {
+        return alarmRepository.findByReceiveUserIdAndIsActivateIsTrue(SecurityUtils.getCurrentUserId()).size();
+    }
+
+}
