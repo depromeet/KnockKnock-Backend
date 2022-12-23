cace2df7f740c45190fdb28135eef6e3db352327
mybloom <baejungeun11@gmail.com>
2022-12-23 14:02:12 +0900

    📑 :: 예약 푸쉬알림 전송 시 Reservation 내용 삭제하는 repository 메서드 deleteByIdIn 작성

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/ReservationRepository.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/domain/repository/ReservationRepository.java
@@ -4,9 +4,21 @@ package io.github.depromeet.knockknockbackend.domain.notification.domain.reposit
 import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
 import io.github.depromeet.knockknockbackend.domain.notification.domain.Reservation;
 import io.github.depromeet.knockknockbackend.domain.user.domain.User;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
     List<Reservation> findByGroupAndSendUserOrderBySendAtAsc(Group group, User sendUser);
+
+    List<Reservation> findBySendAtLessThan(LocalDateTime sendAt);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Reservation r where r.id in :reservationIds")
+    void deleteByIdIn(@Param("reservationIds") List<Long> reservationIds);
 }
