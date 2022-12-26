ddf60803ae25e88fa9104d8aea45e350666eb4f8
mybloom <baejungeun11@gmail.com>
2022-12-26 22:48:40 +0900

    ♻ :: QueryNotificationListResponse 에서 groups 삭제
    - 알림방 푸쉬알림 리스트 응답 DTO에서 groups 속성 삭제하고 notificaitons 항목 내에 groups 포함하는 것으로 변경

--- src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/dto/response/QueryNotificationListResponseElement.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/presentation/dto/response/QueryNotificationListResponseElement.java
@@ -1,6 +1,7 @@
 package io.github.depromeet.knockknockbackend.domain.notification.presentation.dto.response;
 
 
+import io.github.depromeet.knockknockbackend.domain.group.presentation.dto.response.GroupBriefInfoDto;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,5 +15,6 @@ public class QueryNotificationListResponseElement {
     private String imageUrl;
     private LocalDateTime createdDate;
     private Long sendUserId;
+    private GroupBriefInfoDto groups;
     private QueryNotificationReactionResponseElement reactions;
 }
