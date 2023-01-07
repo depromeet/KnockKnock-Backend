48e111d2a2153d771ae994363d12f9fce1135e16
mybloom <baejungeun11@gmail.com>
2023-01-07 16:19:27 +0900

    📑 :: NotificationUtils 구현체 생성, sendNotification() 메서드 추가
    - sendNotification() 즉시전송, 예약전송에서 함께 사용하는 로직을 해당 메서드로 통합

---
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/notification/service/NotificationUtilsImpl.java
@@ -0,0 +1,88 @@
+package io.github.depromeet.knockknockbackend.domain.notification.service;
+
+
+import io.github.depromeet.knockknockbackend.domain.group.domain.Group;
+import io.github.depromeet.knockknockbackend.domain.notification.domain.DeviceToken;
+import io.github.depromeet.knockknockbackend.domain.notification.domain.NightCondition;
+import io.github.depromeet.knockknockbackend.domain.notification.domain.Notification;
+import io.github.depromeet.knockknockbackend.domain.notification.domain.repository.NotificationRepository;
+import io.github.depromeet.knockknockbackend.domain.notification.exception.NotificationNotFoundException;
+import io.github.depromeet.knockknockbackend.domain.user.domain.User;
+import io.github.depromeet.knockknockbackend.infrastructor.fcm.FcmService;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class NotificationUtilsImpl implements NotificationUtils {
+
+    private final FcmService fcmService;
+    private final NotificationRepository notificationRepository;
+
+    @Override
+    public Notification queryNotificationById(Long notificationId) {
+        return notificationRepository
+                .findById(notificationId)
+                .orElseThrow(() -> NotificationNotFoundException.EXCEPTION);
+    }
+
+    @Override
+    public void sendNotification(
+            Long sendUserId,
+            Long groupId,
+            String title,
+            String content,
+            String imageUrl,
+            LocalDateTime reservedAt) {
+        // token 가져오기
+        List<DeviceToken> deviceTokens = getDeviceTokens(groupId, sendUserId);
+        List<String> tokens = getFcmTokens(deviceTokens);
+
+        // 알림 히스토리 저장
+        recordNotification(
+                deviceTokens,
+                title,
+                content,
+                imageUrl,
+                Group.of(groupId),
+                User.of(sendUserId),
+                reservedAt);
+
+        // fcm 전송
+        if (tokens.isEmpty()) {
+            return;
+        }
+        fcmService.sendGroupMessage(tokens, title, content, imageUrl);
+    }
+
+    private List<DeviceToken> getDeviceTokens(Long groupId, Long sendUserId) {
+        Boolean nightOption = null;
+        if (NightCondition.isNight()) {
+            nightOption = true;
+        }
+
+        return notificationRepository.findTokenByGroupAndOptionAndNonBlock(
+                sendUserId, groupId, nightOption);
+    }
+
+    private List<String> getFcmTokens(List<DeviceToken> deviceTokens) {
+        return deviceTokens.stream().map(DeviceToken::getToken).collect(Collectors.toList());
+    }
+
+    private void recordNotification(
+            List<DeviceToken> deviceTokens,
+            String title,
+            String content,
+            String imageUrl,
+            Group group,
+            User sendUser,
+            LocalDateTime createdDate) {
+        Notification notification =
+                Notification.makeNotificationWithReceivers(
+                        deviceTokens, title, content, imageUrl, group, sendUser, createdDate);
+        notificationRepository.save(notification);
+    }
+}
