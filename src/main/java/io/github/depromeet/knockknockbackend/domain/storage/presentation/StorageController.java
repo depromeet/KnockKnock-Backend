122ae5143d230ab91721e3dd9febcea250602f85
mybloom <baejungeun11@gmail.com>
2023-01-10 17:19:40 +0900

    📑 :: 보관함 삭제 API requestBody Valid 적용
    - List 사이즈 1이상이도록 @Size(min = 1)설정

--- src/main/java/io/github/depromeet/knockknockbackend/domain/storage/presentation/StorageController.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/storage/presentation/StorageController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 @SecurityRequirement(name = "access-token")
 @Tag(name = "보관함 관련 컨트롤러", description = "")
@@ -44,7 +45,7 @@ public class StorageController {
     @Operation(summary = "보관함에 저장한 푸쉬알림 삭제")
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping
-    public void deleteNotificationFromStorage(@RequestBody DeleteStorage request) {
+    public void deleteNotificationFromStorage(@Valid @RequestBody DeleteStorage request) {
         storageService.deleteNotificationFromStorage(request);
     }
 }
--- src/main/java/io/github/depromeet/knockknockbackend/domain/storage/presentation/dto/request/DeleteStorage.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/storage/presentation/dto/request/DeleteStorage.java
@@ -3,8 +3,12 @@ package io.github.depromeet.knockknockbackend.domain.storage.presentation.dto.re
 
 import java.util.List;
 import lombok.Getter;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 public class DeleteStorage {
+    @Size(min = 1)
+    @NotNull
     List<Long> storageIds;
 }
