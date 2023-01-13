0794f2b76e16fbfa1e8293686da8bbdb72fc7d66
Leeseojune <sung07288346@gmail.com>
2023-01-13 22:19:26 +0900

    🐛 :: Relation Entity

--- src/main/java/io/github/depromeet/knockknockbackend/domain/relation/domain/Relation.java
+++ src/main/java/io/github/depromeet/knockknockbackend/domain/relation/domain/Relation.java
@@ -5,19 +5,25 @@ import io.github.depromeet.knockknockbackend.domain.user.domain.User;
 import io.github.depromeet.knockknockbackend.domain.user.domain.vo.UserInfoVO;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @Table(name = "tbl_relation")
 @Entity
 public class Relation {
 
-    @Id private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private boolean isFriend;
 
